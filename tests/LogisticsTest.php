ebe3d15dc8cd1ade081a7081b0449acf1d56ba93
uuk020 <893609357@qq.com>
2018-12-25 23:25:51 +0800

    初始化单元测试

---
+++ tests/LogisticsTest.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: WytheHuang
+ * Date: 2018/12/25
+ * Time: 23:07
+ */
+
+namespace Wythe\Logistics\Tests;
+
+use Wythe\Logistics;
+use PHPUnit\Framework\TestCase;
+
+class LogisticsTest extends TestCase
+{
+    public function testGetSendingTime()
+    {
+
+    }
+
+    public function testGetLogistics()
+    {
+
+    }
+}
