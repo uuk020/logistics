5dea938e9ab224f74f2b2fe525cd45d0ac4a8fc9
uuk020 <893609357@qq.com>
2019-01-06 18:05:34 +0800

    完成单元测试

--- tests/LogisticsTest.php
+++ tests/LogisticsTest.php
@@ -31,14 +31,19 @@ class LogisticsTest extends TestCase
         $l = new Logistics($f);
         $this->expectException(InvalidArgumentException::class);
         $this->expectExceptionMessage('code arguments cannot empty.');
-        $l->getLogistics('', '');
-        $this->expectException(NoQueryAvailableException::class);
-        $this->expectExceptionMessage('sorry! no query class available');
-        $l->getLogistics('1231231231', '', ['KuaidiBird']);
-        $this->fail('Failed to assert getLogistics throw exception with invalid argument.');
+        $l->getLogisticsByName('', '');
     }
 
-    public function testGetLogisticsForBaidu()
+    public function testGetLogisticsWithQueryClass()
+    {
+        $f = new Factory();
+        $l = new Logistics($f);
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('Class "Wythe\Logistics\Query\KuaidiBirdQuery" not exists.');
+        $l->getLogisticsByName('123213212', 'kuaidiBird');
+    }
+
+    public function testGetLogisticsByBaidu()
     {
         $response = [
             'status' => 0,
@@ -56,26 +61,12 @@ class LogisticsTest extends TestCase
         $baidu->shouldReceive('callInterface')->andReturn($response);
         $factory->shouldReceive('getInstance')->andReturn($baidu);
         $logistics = new Logistics($factory);
-        $this->assertSame($response, $logistics->getLogistics('12312211'));
+        $this->assertSame($response, $logistics->getLogisticsByName('12312211', 'baidu'));
     }
 
-    // TODO
-    public function testGetCompanyCode()
+    public function testGetLogisticsByKuaidi100()
     {
-
-    }
-
-    public function testGetLogisticsForKuaidi100()
-    {
-        $response = [
-            [
-                'status' => 0,
-                'message'  => '',
-                'error_code' => '',
-                'data' => '',
-                'logistics_company' => '',
-                'logistics_bill_no' => ''
-            ],
+        $kuaidiResponse = [
             [
                 'status' => 0,
                 'message'  => '',
@@ -91,15 +82,41 @@ class LogisticsTest extends TestCase
         ];
         $factory = \Mockery::mock(Factory::class);
         $kuaidi = \Mockery::mock(Kuaidi100Query::class);
-        $kuaidi->shouldReceive('callInterface')->andReturn($response);
+        $kuaidi->shouldReceive('callInterface')->andReturn($kuaidiResponse);
         $factory->shouldReceive('getInstance')->andReturn($kuaidi);
         $logistics = new Logistics($factory);
-        $this->assertSame($response, $logistics->getLogistics('12312211'));
+        $this->assertSame($kuaidiResponse, $logistics->getLogisticsByName('12312211', 'kuaidi100'));
     }
 
-    //TODO
     public function testGetLogisticsBoth()
     {
-
+        $response = [
+            [
+                'from' => 'baidu',
+                'exception' => '查询不到数据'
+            ],
+            [
+                'from' => 'kuaidi100',
+                'info' => [
+                    [
+                        'status' => 0,
+                        'message'  => '',
+                        'error_code' => 0,
+                        'data' => [
+                            ['time' => '1545444420', 'desc' => '仓库-已签收'],
+                            ['time' => '1545441977', 'desc' => '广东XX服务点'],
+                            ['time' => '1545438199', 'desc' => '广东XX转运中心']
+                        ],
+                        'logistics_company' => '申通快递',
+                        'logistics_bill_no' => '12312211'
+                    ]
+                ]
+            ]
+        ];
+        $factory = new Factory();
+        $logistics = \Mockery::mock(Logistics::class);
+        $logistics->shouldReceive('getLogisticsByArray')->andReturn($response);
+        $this->assertSame($response, $logistics->getLogisticsByArray('12312211', ['baidu', 'kuaidi100']));
     }
+
 }
